5ccdd6fedb4807bf53f3fea85d96a5d93710c6b3
Pietro Bertera <pietro@bertera.it>
2016-09-17 23:11:24 +0200

    SIGINT handling

--- include/defines.h
+++ include/defines.h
@@ -33,6 +33,7 @@ extern "C" {
 
 #define EXIT_TEST_OK               0
 #define EXIT_TEST_FAILED           1
+#define EXIT_TEST_INTERRUPTED      96
 #define EXIT_TEST_RES_INTERNAL     97
 #define EXIT_TEST_RES_UNKNOWN      98
 #define EXIT_OTHER                 99
